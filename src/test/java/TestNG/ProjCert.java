7aa90a5d55143b1c8055b8b69b5ca63607897c1b
tusha <tusha@DESKTOP-6S0EBQI.Dlink>
2020-04-15 16:45:21 +0530

    print the text extracted

--- src/test/java/TestNG/ProjCert.java
+++ src/test/java/TestNG/ProjCert.java
@@ -20,14 +20,17 @@ public class ProjCert {
 		Thread.sleep(2000);
 		
 		driver.findElement(By.linkText("About Us")).click();
-		Thread.sleep(2000);
+		
 				
 		String estr = "This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" + 
 				"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 		String astr = driver.findElement(By.xpath("/html/body")).getText();
 		Assert.assertEquals(astr, estr);
 		
-		//driver.close();
+		System.out.printf("PRINTING THE EXTRACTED TEXT", astr);
+				
+		Thread.sleep(30000);
+		driver.close();
   }
   
 }
